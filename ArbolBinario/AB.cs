fa6e18e8b4fd083fb1903a148096401494b6f40e
juan27101411 <133291467+juan27101411@users.noreply.github.com>
2025-05-03 08:32:41 -0600

    Add project files.

---
+++ ArbolBinario/AB.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    class AB
+    {
+        public NodoArbol? NodoRaiz { get; set; }
+
+        string respuesta;
+
+        public AB()
+
+        {
+            NodoRaiz = null;
+        }
+
+        bool IsEmpty() => NodoRaiz == null;
+
+
+        public void PoblarArbol(NodoArbol nodoActual)
+        {
+            //verificar si el arbol esrta vacio
+            if(IsEmpty())
+            {
+                nodoActual = CrearNodo();
+                NodoRaiz = nodoActual;
+
+            }
+            //verificar si existe nod por la izq del nodo actual
+            Console.WriteLine($"Existe nodo por la rama izquierda de {nodoActual.Informacion}?");
+            respuesta = Console.ReadLine().Trim().ToLower();
+
+
+            if(respuesta.Equals("s"))
+            {
+                nodoActual.RamaIzquierda = CrearNodo();
+                PoblarArbol(nodoActual.RamaIzquierda);
+
+            }
+           
+            //verificar si existe nod por laddder del nodo actual
+            Console.WriteLine($"Existe nodo por la rama derecha de {nodoActual.Informacion}?");
+            respuesta = Console.ReadLine().Trim().ToLower();
+
+
+            if (respuesta.Equals("s"))
+            {
+                nodoActual.RamaDerecha = CrearNodo();
+                PoblarArbol(nodoActual.RamaDerecha);
+
+            }
+
+
+
+
+        }
+
+        NodoArbol CrearNodo()
+        {
+            NodoArbol nuevoNodo = new NodoArbol();
+            Console.WriteLine("Ingrese info para el nodo");
+            nuevoNodo.Informacion = Console.ReadLine();
+            return nuevoNodo;
+        }
+    }
+}
---
+++ ArbolBinario/NodoArbol.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    class NodoArbol
+    {
+        public NodoArbol? RamaIzquierda { get; set; }
+        public object? Informacion { get; set; }
+        public NodoArbol? RamaDerecha { get; set; }
+
+        public NodoArbol()
+        {
+            RamaIzquierda = null;
+            Informacion = null;
+            RamaDerecha = null;
+        }
+
+        public NodoArbol(object info)
+        {
+            RamaIzquierda = null;
+            Informacion = null;
+            RamaDerecha = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Informacion}";
+        }
+
+    }
+}
